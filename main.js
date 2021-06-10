6959f1aba4cbdf460a292bb97ad3e57f31821a42
naziiriah <naziistry7@gmail.com>
2021-06-10 19:24:41 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+document.getElementById('quest1').addEventListener('click', open())
+var slide = document.getElementById('1')
+
+function open(e){
+    slide.style.display = 'block'
+
+
+    e.preventDefault
+}
